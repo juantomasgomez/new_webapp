c58af0f42d6888e963071f062dc203c475a03543
juan_tomas <juantgomez5@gmail.com>
2023-05-19 23:04:02 +0100

    Implement 'Add new items' webapp (Day 19)

--- web.py
+++ web.py
@@ -1,18 +1,27 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + '\n'
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title('My Todo App')
 st.subheader('This is my to-do app')
 st.write('This is the first web-app I have ever coded')
 
-todos = functions.get_todos()
 
 for i in todos:
     st.checkbox(i)
 
-st.text_input(label="", placeholder="Add new todo")
+st.text_input(label="", placeholder="Add new todo",
+              on_change=add_todo, key='new_todo')
 
-
+st.session_state
 
 
 
